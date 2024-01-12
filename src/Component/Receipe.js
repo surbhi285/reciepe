0a2bed351514c88c11f6b5d7a76da2633bbe1a39
surbhi285 <sursingh52@gmail.com>
2024-01-12 22:08:40 +0530

    update

--- src/Component/Receipe.js
+++ src/Component/Receipe.js
@@ -1,60 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import bg from '../Images/bg.jpg';
-import cooking from '../Images/cooking.gif';
-import axios from 'axios';
-import { NavLink} from 'react-router-dom';
-import SearchResult from './SearchResult';
-
-
+import React, {useState } from 'react';
+import bg from '../Images/bg.webp';
+// import cooking from '../Images/cooking.gif';
+import { NavLink, useNavigate} from 'react-router-dom';
 
 export default function Receipe() {
+
+    const[inputValue, setInputValue] = useState('');
     const [searchQuery, setSearchQuery] = useState('');
-    const [result, setResult] = useState([]);
-    
+    const navigate = useNavigate();
 
+    const handleData=()=>{
+        console.log(inputValue);
+        if(inputValue.trim() !==''){
+        setSearchQuery(inputValue);
+        navigate('/SearchResult',{
+        state: {
+            data: inputValue,    
+        }})   
+    }else{
+         console.log("Input value is empty")
+        }
+    }
   
-    async function getSearchList(query){
-        try{
-            const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=b87b97c759664a23bb532c71f8facb2b&query=${query}`)
-             if(response.data.results && response.data.results.length>0){
-                console.log(response.data.results);
-                setResult(response.data.results);
-             }else{
-                setResult([]);
-             }
-        }catch(e){
-            console.log("error");
-        }
-    }
-    useEffect(()=>{
-        if(searchQuery !== ''){
-        getSearchList(searchQuery);
-        }
-    },[searchQuery]);
-
-    const handleSearch = ()=>{
-        getSearchList(searchQuery);
-       
-    }
     return (
         <div className='relative flex justify-center items-center h-screen'>
 
-        <div className='bg-cover bg-center h-full w-full relative' 
+        <div className='bg-cover bg-center h-full w-full relative'   
         style={{ backgroundImage: `url(${bg})` }}>
-
+        <div className='h-full w-full absolute top-0 left-0 bg-black opacity-75'></div>
+        <div className='relative'>
         <div className='mt-40 ml-96 flex'>
-
         <input className='mt-12 ml-20' type="text" placeholder='Search Your Reciepe here' 
-          value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)}
-           style={{height:"3rem", width:"25rem", border:"2px solid brown", borderRadius:"2rem", paddingLeft:"6rem"}}/>
-          </div>
+        value={inputValue} onChange={(e)=>setInputValue(e.target.value)}
+        style={{height:"3rem", width:"25rem", border:"2px solid brown", borderRadius:"2rem", paddingLeft:"6rem"}}/>
+        </div>
+        
         <NavLink to= '/SearchResult'>
-       
-        <button className='mt-12 h-12 w-36 pl-1' onClick={handleSearch}
+        <button className='mt-12 h-12 w-36 pl-1' onClick={handleData}
             style={{backgroundColor:"brown", color:"white", border:"2px solid brown", borderRadius:"10px", marginLeft:"37rem"}}>Search</button>
         </NavLink>
-        <SearchResult data={result} /> 
-        <img src={cooking} alt="cooking" style={{position:"absolute", height:"14rem", right:"10rem", bottom:"5rem"}}/>
+        </div>
+        {/* <img src={cooking} alt="cooking" style={{position:"absolute", height:"14rem", right:"10rem", bottom:"5rem"}}/> */}
         </div>
       </div>
         
--- src/Component/SearchResult.js
+++ src/Component/SearchResult.js
@@ -1,26 +1,77 @@
-import React from "react";
+import axios from "axios";
+import poster from '../Images/poster.avif'
+import React, { useEffect, useState } from "react";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import {NavLink, useLocation} from 'react-router-dom';
 
-const SearchResult=(props)=>{
-    const { data: result } = props;
- console.log(result);
+function SearchResult(){
+    const [result, setResult] = useState([]);
+    const location = useLocation();
+    console.log(location)
 
- if (!result || result.length === 0) {
-    return <p>No result found</p>;
-}
-
+    async function getSearchList(query) {
+      try {
+        if(query){
+        const response = await axios.get(
+          `https://api.spoonacular.com/recipes/complexSearch?apiKey=b87b97c759664a23bb532c71f8facb2b&query=${query}`
+        );
+        if (response.data.results && response.data.results.length > 0) {
+          console.log(response.data.results);
+          setResult(response.data.results);
+        } else {
+          setResult([]);
+        }
+      }else{
+        console.log("No query provided");
+      }
+      } catch (e) {
+        console.log("error");
+      }
+    }
+  
+    useEffect(() => {
+        console.log(location.state)
+      const { state } = location;
+      if (state && state.data) {
+        getSearchList(state.data);
+      }else{
+        console.log("No search data is there")
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location]); 
+    if (!result || result.length === 0) {
+        return <p>No result found</p>;
+      }
+  
     return (
-        <div>
-            <h1>Search Results</h1>
-            {result?.length > 0 ? (
-          <ul>
-            {result.map((data) =>(
-                <li key={data.id}>{data.title}</li>
-            ))}
-          </ul>
-            ):(
-            <p>No result found</p>
-             )}
+      <div>
+        <div className='bg-cover bg-center h-full w-full relative' 
+        style={{ backgroundImage: `url(${poster})` }}>
+        <div className="pt-5">
+        <NavLink to="/Receipe">
+        <button className=" flex flex-row justify-center items-center border-solid border-2 border-blue-400 h-10 ml-10 rounded-full 
+        bg-blue-400 text-white text-lg font-bold w-28">
+        <BiLeftArrowAlt className="mr-3 text-2xl"/> Back
+        </button>
+        </NavLink>
         </div>
-    )
-}
-export default SearchResult;
+        <div className="flex flex-col items-center justify-center pb-20">
+        <h1 className="text-4xl font-bold">SEARCH RESULTS OF {(location.state.data).toUpperCase()}</h1>
+        </div>
+        {result?.length > 0 ? (
+          <div className="flex flex-row flex-wrap justify-around items-center mb-2" >
+            {result.map((data) => (
+              <div key={data.id} className="flex flex-col items-center mb-4 ml-6 mt-10" style={{width: "300px", height: "300px"}}>
+            <img src={data.image} alt="recipe" className="mb-2" style={{ width: "300px", height: "300px" }}/>
+              <p className="text-lg font-bold">{data.title}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No result found</p>
+        )}
+      </div>
+      </div>
+    );
+  }
+  export default SearchResult;
